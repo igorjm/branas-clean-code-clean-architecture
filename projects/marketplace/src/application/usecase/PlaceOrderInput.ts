8ec87d0059079a591a915bddb243b87af5402a92
igor melo <igorjmelo4@gmail.com>
2021-12-13 20:24:17 -0300

    refactoring

---
+++ projects/marketplace/src/application/usecase/PlaceOrderInput.ts
@@ -0,0 +1,4 @@
+export default class PlaceOrderInput {
+	constructor (readonly cpf: string, readonly orderItems: { idItem: number, quantity: number }[], readonly date: Date, readonly coupon?: string) {
+	}
+}
